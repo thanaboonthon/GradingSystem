ab9b2383cddbbca1f220580a30134bc59ea11a15
SUPAT RATTANASUKSUN <supat.r@bu.ac.th>
2023-10-17 00:23:50 +0700

    Add project files.

---
+++ Grading.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystem
+{
+    public class Grading
+    {
+        public static void GradeCalculator (int score)
+        {
+            if (score < 0)
+            {
+                Console.WriteLine("Error: Score is lower than zero" + score);
+            }
+            else
+            {
+                if (score >= 80 && score < 100)
+                {
+                    Console.WriteLine("Grade: A (" + score + ")");
+                }
+                else
+                {
+                    if (score >= 70)
+                    {
+                        Console.WriteLine("Grade: B (" + score + ")");
+                    }
+                    else
+                    {
+                        if (score >= 60)
+                        {
+                            Console.WriteLine("Grade: C (" + score + ")");
+                        }
+                        else
+                        {
+                            if (score >= 50)
+                            {
+                                Console.WriteLine("Grade: D (" + score + ")");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Grade: F (" + score + ")");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GradingSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter score: ");
+            int score = Convert.ToInt32(Console.ReadLine());
+            Grading.GradeCalculator(score);
+        }
+    }
+}
